8a17109d41dc3d586c7cecf73e599c6af3350a73
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-06-24 08:17:07 +0300

    Styling is added to the list of the books

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -42,21 +42,13 @@ export default function AddBook() {
 
   return (
     <Form onSubmit={handleSubmit}>
-      {/* <form onSubmit={handleSubmit}> */}
       <Form.Group controlId="formBasicText" className="custom-form">
         <Form.Control type="text" placeholder="Title" name="title" value={book.title} onChange={handleInputChange} required />
         <Form.Control type="text" placeholder="Author" name="author" value={book.author} onChange={handleInputChange} required />
         <Button variant="primary" type="submit">
-          Add book
+          ADD BOOK
         </Button>
       </Form.Group>
-      {/* <input type="text" placeholder="Title" name="title" value={book.title}
-      onChange={handleInputChange} required />
-      <input type="text" placeholder="Author" name="author" value={book.author}
-      onChange={handleInputChange} required /> */}
-      {/* <button type="submit">Add book</button> */}
-
-      {/* </form> */}
     </Form>
   );
 }
